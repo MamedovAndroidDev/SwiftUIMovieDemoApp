20b75a35ac9c983c30730f5e851e4171d0bbf804
MamedovAndroidDev <ayaz.mammadov.baku@gmail.com>
2026-09-24 22:45:09 +0400

    App created

---
+++ MovieCatalogTests/MovieCatalogTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCatalogTests.swift
+//  MovieCatalogTests
+//
+//  Created by Ayaz Memmedov on 13.09.26.
+//
+
+import Testing
+@testable import MovieCatalog
+
+struct MovieCatalogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
